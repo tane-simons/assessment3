25c832a9559e30775c714f122a67789bdd71fc01
tane-simons <163289987+tane-simons@users.noreply.github.com>
2024-09-30 15:40:08 +1000

    streamlit initial

--- part1.py
+++ part1.py
@@ -1,5 +1,9 @@
 import pandas as pd
-csv=pd.read_csv("laptop.csv")
-print(csv)
-noduplicate=csv.drop_duplicates()
-print(noduplicate)
+import streamlit as st
+
+st.write("# Assessment 3 - Laptop Dataset")
+
+st.write("## Step 1 - Reading a sample of clean data")
+data=pd.read_csv("laptop.csv")
+clean_dataset=data.drop_duplicates()
+st.dataframe(clean_dataset.sample(10))
