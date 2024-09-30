149d44f1c4d368cc98ff23cb325ed1a7a0860f83
tane-simons <163289987+tane-simons@users.noreply.github.com>
2024-09-30 15:41:38 +1000

    streamlit attempt2

--- part1.py
+++ part1.py
@@ -7,3 +7,5 @@ st.write("## Step 1 - Reading a sample of clean data")
 data=pd.read_csv("laptop.csv")
 clean_dataset=data.drop_duplicates()
 st.dataframe(clean_dataset.sample(10))
+
+st.write("##Step 2 - ")
